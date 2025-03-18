34620253030f991faaaad4ec7000da1d3947ffab
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-03-18 18:22:43 -0500

    bug fix

--- 02-Challenge/Bun_Develop/server/src/server.ts
+++ 02-Challenge/Bun_Develop/server/src/server.ts
@@ -39,10 +39,10 @@ const PORT = process.env.PORT || 4000;
 
 db.once("open", () => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   app.listen(PORT, () => {
